352735201df787343829e22d488537e88b024b6d
cw-nikhil <nikhilrathore794@gmail.com>
2022-10-09 16:51:31 +0530

    initial setup

---
+++ patient-monitoring-system/src/main/java/com/kafkastreams/patientmonitoringsystem/PatientMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kafkastreams.patientmonitoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ patient-monitoring-system/src/test/java/com/kafkastreams/patientmonitoringsystem/PatientMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkastreams.patientmonitoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
