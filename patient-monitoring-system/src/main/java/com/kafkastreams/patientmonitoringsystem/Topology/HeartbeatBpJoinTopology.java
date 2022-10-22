4501414056a857db6a42c44dded3538b7ba6ca6d
cw-nikhil <nikhilrathore794@gmail.com>
2022-10-22 19:53:27 +0530

    materialize joined stream

--- patient-monitoring-system/src/main/java/com/kafkastreams/patientmonitoringsystem/Topology/HeartbeatBpJoinTopology.java
+++ patient-monitoring-system/src/main/java/com/kafkastreams/patientmonitoringsystem/Topology/HeartbeatBpJoinTopology.java
@@ -3,12 +3,15 @@ package com.kafkastreams.patientmonitoringsystem.Topology;
 import java.time.Duration;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.ValueJoiner;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import com.kafkastreams.patientmonitoringsystem.CustomSerdes.JsonSerde;
 import com.kafkastreams.patientmonitoringsystem.Models.BloodPressure;
@@ -19,6 +22,7 @@ public class HeartbeatBpJoinTopology {
     private static String highHeartbeatTopic = "high-heartbeat";
     private static String deviceAvgHeartbeatValues = "device-avg-hb";
     private static String combinedValuesTopic = "bp-hb-topic";
+    private static String patientCombinedStatsStore = "patient-stats";
     private static int joinWindowInSeconds = 30;
     public void run() {
         var builder = new StreamsBuilder();
@@ -39,5 +43,19 @@ public class HeartbeatBpJoinTopology {
             )
         );
         joinedStream.to(combinedValuesTopic, Produced.with(Serdes.String(), new JsonSerde<HbBpJoinedValue>()));
+
+        materializeJoinedStream(joinedStream);
+
+    }
+
+    private void materializeJoinedStream(KStream<String, HbBpJoinedValue> joinedStream) {
+        joinedStream.
+            groupByKey()
+            .count(
+                Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>
+                    as(patientCombinedStatsStore)
+                    .withKeySerde(Serdes.String())
+                    .withValueSerde(Serdes.Long())
+            );
     }
 }
